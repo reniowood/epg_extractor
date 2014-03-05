a44d0155b860cf79dc19263050299d7c4a8895ff
Jinhyuk Kim <jhkim7@humaxdigital.com>
2014-03-05 16:49:36 +0900

    corrected get_bits in common.c

--- common.c
+++ common.c
@@ -1,25 +1,35 @@
 #include "common.h"
 
 uint32_t get_bits(uint8_t start, uint8_t size, uint8_t *data) {
-    uint8_t start_byte = start / 8;
-    uint8_t end_byte = (start + size - 1) / 8;
+    uint8_t end = start + size - 1;
+    uint8_t start_byte = start / 8, end_byte = end / 8;
+    uint8_t bit_index, byte_index;
+    uint32_t bits, temp;
 
-    uint32_t bits;
-    uint8_t temp;
+    bits = 0;
+    bit_index = start;
+    while (bit_index <= end) {
+        byte_index = bit_index / 8;
 
-    bits = *(data + start_byte);
-    bits &= ((1 << (8 - (start % 8))) - 1);
-    bits <<= 8 - (start % 8);
+        if (end < (byte_index + 1) * 8) {
+            temp = *(data + byte_index);
+            temp >>= (byte_index + 1) * 8 - end - 1;
+            temp &= ((1 << (end - bit_index + 1)) - 1);
 
-    for (start_byte = start_byte+1; start_byte<end_byte; ++start_byte) {
-        temp = *(data + start_byte);
-        bits |= temp;
-        bits <<= 8;
+            bits <<= end - bit_index + 1;
+            bits |= temp;
+
+            bit_index = end + 1;
+        } else {
+            temp = *(data + byte_index);
+            temp &= ((1 << ((byte_index + 1) * 8 - bit_index)) - 1);
+
+            bits <<= (byte_index + 1) * 8 - bit_index;
+            bits |= temp;
+
+            bit_index += (byte_index + 1) * 8 - bit_index;
+        }
     }
-
-    temp = *(data + end_byte);
-    temp &= ((1 << (8 - (start + size) % 8)) - 1);
-    bits |= temp;
 
     return bits;
 }
