ec358ace4bdd5dba814853da7629148e38eb4f38
Jinhyuk Kim <jhkim7@humaxdigital.com>
2014-03-10 18:19:28 +0900

    메모리 누수 해결, 메모리 잘못된 접근 해결, version_list를 하나로 합침, version 관리 추가...... 이제 그만....

--- common.h
+++ common.h
@@ -15,8 +15,20 @@ struct Identifier {
     uint32_t table_id;
 };
 
-uint64_t get_bits(uint8_t start, uint8_t size, uint8_t *data);
 struct Identifier init_id();
 int compare_id(struct Identifier *id_1, struct Identifier *id_2);
+
+struct Date {
+    uint16_t year, month, day;
+    uint16_t hour, minute, second;
+};
+
+struct Date init_date();
+int compare_date(struct Date *date_1, struct Date *date_2);
+void get_ymd(struct Date *date, uint64_t time);
+void get_hms(struct Date *date, uint64_t time);
+void add_date(struct Date *result, struct Date *date_1, struct Date *date_2);
+
+uint64_t get_bits(uint8_t start, uint8_t size, uint8_t *data);
 
 #endif
