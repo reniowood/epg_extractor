f4148a69a73afcaa4dfaf966d46b947fd190f0ca
jhkim <jhkim@jhkim-VirtualBox.(none)>
2014-07-08 14:12:41 +0900

    express.js의 새로운 version (over 4.0.0)에 맞게 server.js 를 수정

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-var express = require('express'),
-    fs      = require('fs'),
-    cp      = require('child_process');
-var app     = express();
+var express         = require('express'),
+    morgan          = require('morgan'),
+    cp              = require('child_process');
+var app             = express();
 
-app.configure(function () {
-    app.use(express.static(__dirname + '/web_interface'));
-    app.use(express.logger('dev'));
-    app.use(express.methodOverride());
-    app.use(express.bodyParser());
-    app.use(app.router);
-});
+app.use(express.static(__dirname + '/web_interface'));
+app.use(morgan('dev'));
 
 app.get('/showEPG/:TSFileName', function (req, res) {
     var extractor = cp.spawn('extractor/extractor.out', [req.params.TSFileName]);
